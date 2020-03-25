d7f613550371ea76a250e22564d756b945259244
Subin Lee <chandar.sblee@gmail.com>
2020-03-25 12:50:22 +0900

    Add Redis Runner with Account CRUD Repository

--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists account;
-drop sequence if exists hibernate_sequence;
-create sequence hibernate_sequence start with 1 increment by 1;
-create table account (
-    id bigint not null primary key,
-    password varchar(255),
-    username varchar(255)
-);
